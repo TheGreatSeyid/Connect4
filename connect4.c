da02e545a830f9ecd4b01fd320eb0e3d74e34d45
Obanos95 <sgaribov451@gmail.com>
2025-03-10 15:20:42 +0400

    Update connect4.c

--- connect4.c
+++ connect4.c
@@ -8,9 +8,29 @@ void mapViz(char map[][9]){
 			printf("%c ",map[i][j]);
 		printf("\n");
 	}
-
 }
 
+void move(char move, char map[][9]){
+	int column;
+
+	if (move == 'r')
+		puts("Player 1 play");
+	else
+		puts("PLayer 2 play");
+
+	puts("Enter column you want to enter your disk");
+	scanf("%d",&column);
+
+	for (int i = 1;i < 6;i++){
+		if (map[i+1][column+1] != ' '){
+			if (move == 'r')
+				map[i][column+1] = '*';
+			else
+				map[i][column+1] = 'o';
+		}
+	}
+
+}
 
 
 int main(){

28ca98cc80b452da2599b43510123609ecf41ad1
Obanos95 <sgaribov451@gmail.com>
2025-03-10 14:52:43 +0400

    Update connect4.c

--- connect4.c
+++ connect4.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 
+void mapViz(char *map){
+	for (int i = 0;i < sizeof(map)/sizeof(map[1]);i++){
+		for (int j = 0;j < sizeof(map[i])/sizeof(map[i][1]);j++)
+			printf("%c",);
+
+	}
+
+}
+
+
 
 int main(){
 	
